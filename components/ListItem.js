1598b5e842e2f3304eccd5662eae0cb6f513bd77
Shayne kandagor <shayne.kandagor@gmail.com>
2023-02-16 10:56:27 +0200

    Added flexbox styles

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,22 +1,45 @@
-import {Image, View, Text, TouchableOpacity} from 'react-native';
+import {Image, View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
 
 const ListItem = ({singleMedia}) => {
   const item = singleMedia;
   return (
-    <TouchableOpacity>
-      <Image
-        style={{width: 100, height: 100}}
-        source={{uri: uploadsUrl + item.thumbnails?.w160}}
-      ></Image>
-      <View>
-        <Text>{item.title}</Text>
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.box}>
+        <Image
+          style={styles.image}
+          source={{uri: uploadsUrl + item.thumbnails?.w160}}
+        />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.listTitle}>{item.title}</Text>
         <Text>{item.description}</Text>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#ccc',
+    marginBottom: 10,
+  },
+  box: {
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    flex: 1,
+    minHeight: 100,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
