0193d7b9b9346041fb0edd731f3b41f00fe57d95
Shayne kandagor <shayne.kandagor@gmail.com>
2023-02-16 20:29:19 +0200

    Added basic tab icons

--- navigators/navigator.js
+++ navigators/navigator.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Upload from '../views/Upload';
 import Search from '../views/Search';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Single from '../views/Single';
 
 const Tab = createBottomTabNavigator();
@@ -13,7 +14,25 @@ const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => {
+        return {
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'person';
+            } else if (route.name === 'Upload') {
+              iconName = 'cloud-upload';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            }
+            return <Ionicons name={iconName} />;
+          },
+        };
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Upload" component={Upload} />
