e4cc027d174404c9c88cfd5899f3b76763702304
Shayne kandagor <shayne.kandagor@gmail.com>
2023-02-18 20:35:36 +0200

    Display basic user profile image

--- views/Profile.js
+++ views/Profile.js
@@ -1,13 +1,33 @@
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('');
+
+  const loadAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      // Getting the latest profile if you have uploaded many files
+      setAvatar(avatarArray.pop().filename);
+    } catch (error) {
+      console.error('user avatar failed', error.message);
+    }
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Image style={styles.image} source={{uri: uploadsUrl + avatar}} />
       <Text>Full Name: {user.full_name}</Text>
       <Text>Username: {user.username}</Text>
       <Text>Email: {user.email}</Text>
@@ -36,6 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
 
