2ab63538a19fc07dadb598b78df8ebb1993bba6d
Shayne kandagor <shayne.kandagor@gmail.com>
2023-02-21 11:59:26 +0200

    User is able to modify own files

--- components/ListItem.js
+++ components/ListItem.js
@@ -49,7 +49,7 @@ const ListItem = ({singleMedia, navigation}) => {
             rounded
             onPress={(index) => {
               if (index === 0) {
-                console.log('Modify', {file: item});
+                navigation.navigate('Modify', {file: item});
               } else {
                 doDelete();
               }
