992d47468a319fcdd3f6a800293e32425ff9369f
Shayne kandagor <shayne.kandagor@gmail.com>
2023-02-22 14:45:40 +0200

    Added a basic functional screen

--- views/WelcomeScreen.js
+++ views/WelcomeScreen.js
@@ -1,31 +1,48 @@
-import {Image, Text} from '@rneui/themed';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import colors from '../config/colors';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 import PropTypes from 'prop-types';
 
+import colors from '../config/colors';
+import {Text} from '@rneui/themed';
+
 function WelcomeScreen({navigation}) {
+  const handleRegistrationPress = () => {
+    navigation.navigate('Login');
+  };
+
+  const handleLoginPress = () => {
+    navigation.navigate('Login');
+  };
+
   return (
-    <Image
+    <ImageBackground
       style={styles.background}
       source={require('../assets/welcome_img.jpeg')}
     >
       <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require('../assets/renewlogo.jpg')}
-        />
-        <Text color="white">Giving Second Home to Great Stuff</Text>
+        <Text style={styles.logoText}>Giving Second Home to Great Stuff</Text>
       </View>
-      <View style={styles.registerButton}>
-        <Text>Create an account</Text>
-      </View>
-      <View style={styles.loginButton}>
-        <Text>Sign In</Text>
-      </View>
-    </Image>
+      <TouchableOpacity
+        style={styles.registerButton}
+        onPress={handleRegistrationPress}
+      >
+        <Text style={styles.buttonText}>Create an account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLoginPress}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
+
+WelcomeScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
 
 const styles = StyleSheet.create({
   background: {
@@ -33,29 +50,36 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  loginButton: {
-    width: '100%',
-    height: 70,
-    backgroundColor: colors.lightgray,
+  logoContainer: {
+    position: 'absolute',
+    top: 100,
+    alignItems: 'center',
+  },
+  logoText: {
+    color: colors.white,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   registerButton: {
     width: '100%',
     height: 70,
     backgroundColor: colors.secondary,
-  },
-  logo: {
-    width: 150,
-    height: 100,
-  },
-  logoContainer: {
-    position: 'absolute',
-    top: 150,
+    justifyContent: 'center',
     alignItems: 'center',
   },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: colors.lightgray,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
-
-WelcomeScreen.propTypes = {
-  navigation: PropTypes.object,
-};
 
 export default WelcomeScreen;
